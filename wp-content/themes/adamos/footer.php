4838cb056500d4a0af7b478b87b940e63a52444f
Roma48 <myzfann@mail.ru>
2015-02-19 18:59:43 +0200

    contact form

--- wp-content/themes/adamos/footer.php
+++ wp-content/themes/adamos/footer.php
@@ -14,33 +14,35 @@
 	<footer id="colophon" class="site-footer" role="contentinfo">
     <div class="footer_container">
     <div class="section group">
-    
+
 	<div class="col span_1_of_3">
     <?php if ( is_active_sidebar( 'left_column' ) && dynamic_sidebar('left_column') ) : else : ?>
-         <div class="widget">
-			<?php echo '<h4>' . __('Widget Ready', 'adamos') . '</h4>'; ?>
-            <?php echo '<p>' . __('This left column is widget ready! Add one in the admin panel.', 'adamos') . '</p>'; ?>
-            </div>     
-	<?php endif; ?>  
+
+	<?php endif; ?>
 		</div>
         
 	<div class="col span_1_of_3">
 	<?php if ( is_active_sidebar( 'center_column' ) && dynamic_sidebar('center_column') ) : else : ?>
-         <div class="widget">
-			<?php echo '<h4>' . __('Widget Ready', 'adamos') . '</h4>'; ?>
-            <?php echo '<p>' . __('This center column is widget ready! Add one in the admin panel.', 'adamos') . '</p>'; ?>
-            </div>     
+        <div class="widget">
+            <div id="text-3" class="widget widget_text">
+                <h4>Як нас знайти?</h4>
+            </div>
+            <?php wd_contact_form_maker(11); ?>
+        </div>
 	<?php endif; ?> 
 
 	</div>
     
 	<div class="col span_1_of_3">
 	<?php if ( is_active_sidebar( 'right_column' ) && dynamic_sidebar('right_column') ) : else : ?>
-         <div class="widget">
-			<?php echo '<h4>' . __('Widget Ready', 'adamos') . '</h4>'; ?>
-            <?php echo '<p>' . __('This right column is widget ready! Add one in the admin panel.', 'adamos') . '</p>'; ?>
-            </div>     
-	<?php endif; ?> 
+
+        <div class="widget">
+            <div id="text-3" class="widget widget_text">
+                <h4>Зворотній звязок</h4>
+            </div>
+            <?php wd_contact_form_maker(10); ?>
+        </div>
+	<?php endif; ?>
 	</div>
 	</div>
         </div><!-- footer container -->
--- wp-content/themes/adamos/page-templates/custom_home.php
+++ wp-content/themes/adamos/page-templates/custom_home.php
@@ -7,11 +7,12 @@
  * @since adamos 1.0
  */
 
-get_header(); ?>
+get_header('no-slider'); ?>
         
-        <div id="primary_home" class="content-area">
+        <div id="primary_home" style="margin-top: 20px" class="content-area">
 			<div id="content" class="fullwidth" role="main">
-  
+    <div  class="entry-title">Наші Послуги</div>
+                
    <div class="section group">
 	<div class="col span_1_of_3">         
     <div class="featuretext">
@@ -35,6 +36,28 @@ get_header(); ?>
                 <img style="width: 250px; height: 200px" src="/wp-content/themes/adamos/images/3.jpg" />
 	</div>
     </div>
+       <div class="col span_1_of_3" style="margin-left: 0">
+           <div class="featuretext">
+               <h3><?php echo get_theme_mod( 'featured_textbox_header_one' ); ?></h3>
+               <img style="width: 250px; height: 200px" src="/wp-content/themes/adamos/images/1.jpg" />
+           </div>
+       </div>
+
+
+
+       <div class="col span_1_of_3">
+           <div class="featuretext">
+               <h3><?php echo get_theme_mod( 'featured_textbox_header_two' ); ?></h3>
+               <img style="width: 250px; height: 200px" src="/wp-content/themes/adamos/images/2.jpg" />
+           </div>
+       </div>
+
+       <div class="col span_1_of_3">
+           <div class="featuretext">
+               <h3><?php echo get_theme_mod( 'featured_textbox_header_three' ); ?></h3>
+               <img style="width: 250px; height: 200px" src="/wp-content/themes/adamos/images/3.jpg" />
+           </div>
+       </div>
        <div class="col span_1_of_3" style="margin-left: 0 ">
            <div class="featuretext">
                <h3><?php echo get_theme_mod( 'featured_textbox_header_four' ); ?></h3>
@@ -59,30 +82,7 @@ get_header(); ?>
 
     </div>
     
-     <div class="section_thumbnails group">
-	<?php echo '<h3>' . __('Recent Posts', 'adamos') . '</h3>'; ?>
 
-  <?php $the_query = new WP_Query(array(
-  'showposts' => 2,
-  'post__not_in' => get_option("sticky_posts"),
-  ));
- ?>
-    <?php while ($the_query -> have_posts()) : $the_query -> the_post(); ?>
-    <div class="col span_1_of_2">
-    <article class="recent">
-    			<h2><a href="<?php the_permalink(); ?>"><?php the_title(); ?></a></h2>
-                <?php
-			if ( has_post_thumbnail() ) {
-    $image_src = wp_get_attachment_image_src( get_post_thumbnail_id(),'featured' );
-     echo '<img alt="post" class="imagerct" src="' . $image_src[0] . '">';
-}
-  			?>
-                <?php echo adamos_content(50); ?><div class="thumbs-more-link"><a href="<?php the_permalink() ?>"> <?php echo __('More', 'adamos'); ?></a></div>
-    </article>
-    </div>			
-	<?php endwhile; ?>
-
-    </div>
     
 			</div><!-- #content .site-content -->
 		</div><!-- #primary .content-area -->
